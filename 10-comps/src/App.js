7675dced902def47bbe8223afd5ff1ad5d025fcc
Jesse Lai <nhathoang8c01@gmail.com>
2023-01-02 11:28:28 +0700

    create custom components with tailwindCSS

---
+++ 10-comps/src/App.js
@@ -0,0 +1,47 @@
+import Button from "./Button";
+import { GoBell, GoCloudDownload, GoDatabase } from "react-icons/go";
+
+function App() {
+  const handleOnClick = () => {
+    console.log("test");
+  };
+
+  return (
+    <div>
+      <div>
+        <Button
+          secondary
+          outline
+          rounded
+          onClick={handleOnClick}
+          className="mb-5"
+        >
+          <GoBell />
+          Handsome
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoCloudDownload />
+          Buy now
+        </Button>
+      </div>
+      <div>
+        <Button warning outline>
+          <GoDatabase />
+          Another button
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hello mah fens
+        </Button>
+      </div>
+      <Button primary rounded outline>
+        Let's go Link Chou
+      </Button>
+    </div>
+  );
+}
+
+export default App;
