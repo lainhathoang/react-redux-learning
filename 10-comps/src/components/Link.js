6827db6bb3b22f8ffe6cd4ed6c92498aa083284a
Jesse Lai <nhathoang8c01@gmail.com>
2023-01-06 23:11:44 +0700

    implement navigation & react-router from scratch

--- 10-comps/src/components/Link.js
+++ 10-comps/src/components/Link.js
@@ -1,17 +1,31 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+import classNames from "classnames";
+import useNavigation from "../hooks/use-navigation";
 
 // implement this component for wrap the url to use with navgiation context
-function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+function Link({ to, children, className, activeClassName }) {
+  const { navigate, currentPath } = useNavigation();
+
+  const classes = classNames(
+    "text-blue-500",
+    className,
+    currentPath === to && activeClassName
+  );
 
   const handlClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault();
 
     navigate(to);
   };
 
-  return <a onClick={handlClick}>{children}</a>;
+  return (
+    <a className={classes} href={to} onClick={handlClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
